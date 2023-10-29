c39c5a2f23379fc06dc06ed16f1dc96eb2d2a5af
phnaharris <phnanh.harris@gmail.com>
2023-10-29 10:40:35 +0700

    fix(audit): pve001
    max amount at a time

--- Chat3_V1.sol
+++ Chat3_V1.sol
@@ -193,8 +193,8 @@ contract Chat3_V1 is Ownable {
         whitelist[to] = isWhitelisted;
     }
 
-    function getPrice(uint256 supply, uint256 amount) public pure returns (uint256) {
-        require(amount == 1, "only one at a time");
+    function getPrice(uint256 supply, uint256 amount) public view returns (uint256) {
+        require(amount <= maxAmountAtATime, "amount exceeded");
         uint256 sum1 = supply == 0 ? 0 : (supply - 1 )* (supply) * (2 * (supply - 1) + 1) / 6;
         uint256 sum2 = supply == 0 && amount == 1 ? 0 : (supply - 1 + amount) * (supply + amount) * (2 * (supply - 1 + amount) + 1) / 6;
         uint256 summation = sum2 - sum1;
